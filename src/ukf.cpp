8b4988ce5d6ef62f42c33f5f3741e920ca67cc2b
Nick <nicholas.condo@gmail.com>
2017-05-03 03:51:34 -0400

    fix: Fix typos of member variables missing trailing underscore

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -88,6 +88,7 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
   Complete this function! Make sure you switch between lidar and radar
   measurements.
   */
+
 }
 
 /**
@@ -104,7 +105,7 @@ void UKF::Prediction(double delta_t) {
   x_aug(6) = 0;
 
   // create augmented covariance matrix
-  VectorXd P_aug = VectorXd(n_aug_, n_aug_);
+  MatrixXd P_aug = MatrixXd(n_aug_, n_aug_);
   P_aug.fill(0.0);
   P_aug.topLeftCorner(5,5) = P_;
   P_aug(5,5) = std_a_ * std_a_;
@@ -119,8 +120,8 @@ void UKF::Prediction(double delta_t) {
   // create augmented sigma points
   Xsig_aug.col(0) = x_aug;
   for (int i = 0; i < n_aug_; i++) {
-    Xsig_aug.col(i+1) = x_aug + sqrt(lambda+n_aug_) * L.col(i);
-    Xsig_aug.col(i+1+n_aug_) = x_aug - sqrt(lambda+n_aug_) * L.col(i);
+    Xsig_aug.col(i+1) = x_aug + sqrt(lambda_+n_aug_) * L.col(i);
+    Xsig_aug.col(i+1+n_aug_) = x_aug - sqrt(lambda_+n_aug_) * L.col(i);
   }
 
   // predice sigma points
@@ -139,12 +140,12 @@ void UKF::Prediction(double delta_t) {
 
     // avoid division by zero
     if (fabs(yawd) > 0.001) {
-      px_p = px + v/yawd * (sin(yaw+yawd*delta_t) - sin(yaw));
-      py_p = py + v/yawd * (cos(yaw) - cos(yaw+yawd*delta_t));
+      px_p = p_x + v/yawd * (sin(yaw+yawd*delta_t) - sin(yaw));
+      py_p = p_y + v/yawd * (cos(yaw) - cos(yaw+yawd*delta_t));
     } 
     else {
-      px_p = px + v * delta_t * cos(yaw);
-      py_p = py + v * delta_t * sin(yaw);
+      px_p = p_x + v * delta_t * cos(yaw);
+      py_p = p_y + v * delta_t * sin(yaw);
     }
 
     // predicted velocity, yaw, yawd
@@ -170,20 +171,20 @@ void UKF::Prediction(double delta_t) {
   // predicted state mean
   x_.fill(0.0);
   for (int i = 0; i < 2*n_aug_+1; i++) {
-    x_ = x_ + weights_(i) * Xsig_pred.col(i);
+    x_ = x_ + weights_(i) * Xsig_pred_.col(i);
   }
 
   // predicted state covariance matrix
   P_.fill(0.0);
   for (int i = 0; i < 2*n_aug_+1; i++) {
     // state difference
-    VectorXd x_diff = Xsig_pred.col(i) - x_;
+    VectorXd x_diff = Xsig_pred_.col(i) - x_;
 
     // angle normalization
     while (x_diff(3) > M_PI) x_diff(3) -= 2.*M_PI;
     while (x_diff(3) < -M_PI) x_diff(3) += 2.*M_PI;
 
-    P_ = P_ + weights(i) * x_diff * x_diff.transpose();
+    P_ = P_ + weights_(i) * x_diff * x_diff.transpose();
   }
 
 }
